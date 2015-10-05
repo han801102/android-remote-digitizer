5dbf97e2db232b1332e6a40c0e9a7f3bd3a3069c
han801102 <et801102@gmail.com>
2015-10-05 15:13:40 +0800

    Add basic function: 	1. Connect to PC 	2. Send char and display it

---
+++ app/src/main/java/com/han/remotedigitizer/PadActivity.java
@@ -0,0 +1,99 @@
+package com.han.remotedigitizer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class PadActivity extends AppCompatActivity {
+    private EditText usrInput;
+    private TextView printUsrInput;
+    private String ip;
+    private Socket m_pcSocket;
+    private OutputStream out;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pad);
+        Intent intent = this.getIntent();
+        ip = intent.getStringExtra("ip");
+        Thread mThread = new Thread(mThreadRunnable);
+        mThread.start();
+
+
+        printUsrInput  = (TextView)findViewById(R.id.textView2);
+        usrInput = (EditText)findViewById(R.id.editText2);
+        usrInput.addTextChangedListener(
+                new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        if(before == 1){
+                            try {
+                                byte[] sendstr_big5 = s.toString().getBytes("Big5");
+                                out.write(sendstr_big5);
+                                usrInput.setText("");
+                            }catch (Exception e){
+                                System.out.println("Error:" + e.toString());
+                            }
+                            printUsrInput.setText( s);
+                        }
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {}
+                }
+        );
+    }
+
+    private Runnable mThreadRunnable = new Runnable() {
+        @Override
+        public void run() {
+            int pcPort = 40;
+            try {
+                m_pcSocket = new Socket(ip,pcPort);
+                out = m_pcSocket.getOutputStream();
+            }catch (Exception e){
+                System.out.println("Error:" + e.toString());
+            }
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_pad, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
